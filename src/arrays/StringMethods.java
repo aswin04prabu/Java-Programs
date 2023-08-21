89c63b9b85598d7e35c7aa5ec03f03125be638b2
aswin04prabu <aswinprabu44@gmail.com>
2023-08-21 10:29:42 +0530

    Java Program

---
+++ src/arrays/StringMethods.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class StringMethods {
+	
+	public static void main(String[] args) {
+
+		String s = "Welcome";
+		System.out.println(s.length());
+		
+	}
+
+}
---
+++ src/arrays/TwoDimensionalArray.java
@@ -0,0 +1,44 @@
+package arrays;
+
+public class TwoDimensionalArray {
+	
+	public static void main(String[] args) {
+		
+		int a[][] = new int [3][2]; //declaration
+		
+		//storing elements/values into array
+		
+		a[0][0]=10;
+		a[0][1]=20;
+		
+		a[1][0]=30;
+		a[1][1]=40;
+		
+		a[2][0]=50;
+		a[2][1]=60;
+		
+		//classic for loop
+		/*for(int i=0;i<=2;i++) { // for incrementing the rows
+			
+			for (int j=0;j<=1;j++) { //for incrementing the columns
+				
+				System.out.print(a[i][j]+"  "); //10 20 30 40 50 60
+			}
+			System.out.println();
+		}*/
+		
+		//for..each loop
+		
+		for(int i[]:a) {
+			
+			for(int j:i) {
+				
+				System.out.print(j+"  ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
+}
